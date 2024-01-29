e08ff32d6e78f57d3e775ca49cd78b5948239c8d
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2024-01-29 11:16:07 +0700

    fix login, api

--- app/(authenticate)/register.js
+++ app/(authenticate)/register.js
@@ -9,22 +9,105 @@ import {
     TouchableWithoutFeedback,
     StatusBar
 } from 'react-native'
-import React, { useState } from 'react'
-import { colors } from "../../src/constants/colors";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
-import LottieView from "lottie-react-native";
+import React, {useEffect, useState} from 'react'
+import {colors} from "../../src/constants/colors";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {useRouter} from "expo-router";
+import isEmail from "validator/es/lib/isEmail";
 
 const register = () => {
     const [showPassword, setShowPassword] = useState(false)
+    const [name, setName] = useState('');
+    const [errorName, setErrorName] = useState('');
+    const [email, setEmail] = useState('');
+    const [errorEmail, setErrorEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorPassword, setErrorPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorConfirmPassword, setErrorConfirmPassword] = useState('');
+    const [enableLogin, setEnableLogin] = useState(false);
+
+    // validate name
+    const validateName = () => {
+        if (name === '') {
+            setErrorName('*Name is required');
+            return false;
+        }
+        setErrorName('');
+        return true;
+    };
+
+    // validate email
+    const validateEmail = () => {
+        if (email === '') {
+            setErrorEmail('*Email is required');
+            return false;
+        }
+        if (!isEmail(email)) {
+            setErrorEmail('*Invalid email');
+            return false;
+        }
+        setErrorEmail('');
+        return true;
+    };
+
+    // validate password
+    const validatePassword = () => {
+        if (password === '') {
+            setErrorPassword('*Password is required');
+            return false;
+        } else if (password.length < 6) {
+            setErrorPassword('*Password must be at least 6 characters');
+            return false;
+        }
+        setErrorPassword('');
+        return true;
+    };
+
+    // validate confirm password
+    const validateConfirmPassword = () => {
+        if (confirmPassword === '') {
+            setConfirmPassword('*Confirm password is required');
+            return false;
+        } else if (confirmPassword !== password) {
+            setConfirmPassword('*Confirm password does not match');
+            return false;
+        }
+        setConfirmPassword('');
+        return true;
+    }
+
+    // validate form
+    const validateForm = () => {
+        if (!validateName() || !validateEmail() || !validatePassword() || !validateConfirmPassword()) {
+            return false;
+        }
+        return true;
+    }
+
+    useEffect(() => {
+        if (name !== '' && email !== '' && password !== '' && confirmPassword !== '') {
+            setEnableLogin(true);
+        } else {
+            setEnableLogin(false);
+        }
+    }, [name, email, password, confirmPassword]);
+
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
     }
     const router = useRouter();
 
+    const handleRegister = () => {
+        if (!validateForm()) {
+            return;
+        }
+        router.replace("/(tabs)/home");
+    }
+
     const handleLogin = () => {
         router.replace("/(authenticate)/login");
-    }
+    };
 
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -39,7 +122,7 @@ const register = () => {
                     barStyle="dark-content"
                 />
 
-                <Image source={require('../assets/images/logo.png')} style={{ width: 80, height: 80, marginTop: 50 }} />
+                <Image source={require('../assets/images/logo.png')} style={{width: 80, height: 80, marginTop: 50}}/>
                 <Text style={{
                     fontSize: 22,
                     color: colors.backgroundColor,
@@ -59,48 +142,81 @@ const register = () => {
                 </Text>
 
                 <View style={{
-                    width: '85%'
+                    width: '85%',
+                    flexDirection: 'column',
                 }}>
                     <TextInput placeholderTextColor={colors.title}
-                        placeholder={'Name'}
-                        cursorColor={colors.orange}
-                        style={{
-                            width: '100%',
-                            borderWidth: 2,
-                            borderColor: colors.title,
-                            padding: 12,
-                            borderRadius: 10,
-                            color: colors.title,
-                            fontWeight: 'bold',
-                            fontSize: 18
-                        }} />
+                               placeholder={'Name'}
+                               value={name}
+                               onChangeText={text => setName(text)}
+                               keyboardType={"default"}
+                               cursorColor={colors.orange}
+                               focusable={
+                                   errorName ? false : true
+                               }
+                               autoFocus={
+                                      errorName ? false : true
+                               }
+                               style={{
+                                   width: '100%',
+                                   borderWidth: 2,
+                                   borderColor: errorName ? 'red' : colors.title,
+                                   padding: 12,
+                                   borderRadius: 10,
+                                   color: colors.title,
+                                   fontSize: 18
+                               }}/>
+                    {errorName ? (
+                        <Text style={{
+                            color: 'red',
+                            fontSize: 12,
+                        }}>
+                            {errorName}
+                        </Text>
+                    ) : null}
                 </View>
                 <View style={{
                     width: '85%',
                     marginVertical: 10
                 }}>
                     <TextInput placeholderTextColor={colors.title}
-                        placeholder={'Email Address'}
-                        keyboardType={"email-address"}
-                        cursorColor={colors.orange}
-                        style={{
-                            width: '100%',
-                            borderWidth: 2,
-                            borderColor: colors.title,
-                            padding: 12,
-                            borderRadius: 10,
-                            color: colors.title,
-                            fontWeight: 'bold',
-                            fontSize: 18
-                        }} />
+                               placeholder={'Email Address'}
+                               keyboardType={"email-address"}
+                               textContentType={"emailAddress"}
+                               value={email}
+                               onChangeText={text => setEmail(text)}
+                               onSubmitEditing={() => Keyboard.dismiss()}
+                               cursorColor={colors.orange}
+                               style={{
+                                   width: '100%',
+                                   borderWidth: 2,
+                                   borderColor: errorEmail ? 'red' : colors.title,
+                                   padding: 12,
+                                   borderRadius: 10,
+                                   color: colors.title,
+                                   fontSize: 18
+                               }}/>
+                    {errorEmail ? (
+                        <Text style={{
+                            color: 'red',
+                            fontSize: 12,
+                        }}>
+                            {errorEmail}
+                        </Text>
+                    ) : null}
                 </View>
+                <View>
                 <View style={styles.container}>
                     <TextInput
                         secureTextEntry={!showPassword}
-                        style={styles.input}
+                        style={[styles.input, {
+                            borderColor: errorPassword ? 'red' : colors.title,
+                        }]}
                         placeholder="Password"
                         placeholderTextColor={colors.title}
                         cursorColor={colors.orange}
+                        value={password}
+                        onChangeText={text => setPassword(text)}
                     />
                     <MaterialCommunityIcons
                         name={showPassword ? 'eye-off' : 'eye'}
@@ -109,14 +225,32 @@ const register = () => {
                         style={styles.icon}
                         onPress={toggleShowPassword}
                     />
+
                 </View>
+                {
+                    errorPassword ? (
+                        <Text style={{
+                            color: 'red',
+                            fontSize: 12,
+                            marginBottom: 10,
+                        }}>
+                            {errorPassword}
+                        </Text>
+                    ) : null
+                }
+                </View>
+                <View>
                 <View style={styles.container}>
                     <TextInput
                         secureTextEntry={!showPassword}
-                        style={styles.input}
+                        style={[styles.input, {
+                            borderColor: errorPassword ? 'red' : colors.title,
+                        }]}
                         placeholder="Re-type password"
                         placeholderTextColor={colors.title}
                         cursorColor={colors.orange}
+                        value={confirmPassword}
+                        onChangeText={text => setConfirmPassword(text)}
                     />
                     <MaterialCommunityIcons
                         name={showPassword ? 'eye-off' : 'eye'}
@@ -126,20 +260,31 @@ const register = () => {
                         onPress={toggleShowPassword}
                     />
                 </View>
-                <TouchableOpacity style={styles.touchable} activeOpacity={0.6} onPress={handleLogin}>
+                {
+                    errorConfirmPassword ? (
+                        <Text style={{
+                            color: 'red',
+                            fontSize: 12,
+                        }}>
+                            {errorConfirmPassword}
+                        </Text>
+                    ) : null
+                }
+            </View>
+                <TouchableOpacity style={styles.touchable} activeOpacity={0.6} onPress={handleRegister}>
                     <Text style={styles.text}>
                         Register
                     </Text>
                 </TouchableOpacity>
-                <Text onPress={() => handleLogin} style={[styles.text, { fontSize: 16, color: colors.title }]}>
-                    You have an account? Click <Text onPress={() => {
-                        router.push('/(authenticate)/login');
-                    }} style={{ color: colors.orange }}
-                    >Login</Text>
+                <Text onPress={() => handleLogin} style={[styles.text, {fontSize: 16, color: colors.title}]}>
+                    You have an account? Click<TouchableOpacity onPress={() => {
+                    router.push('/(authenticate)/login');
+                }}><Text style={{color: colors.orange}}
+                >Login</Text></TouchableOpacity>
                 </Text>
 
             </View>
-        </TouchableWithoutFeedback >
+        </TouchableWithoutFeedback>
     );
 
 }
---
+++ app/(stacks)/search/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+    return (
+        <View>
+            {/* Form contact include  */}
+        </View>
+    )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
